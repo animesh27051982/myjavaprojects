c6f47889d1bd46858b1d614a423fa5a5f3cc310b
Steve Pan <Steve@Steve-PC>
2018-07-14 19:48:05 -0400

    upload ms access data

--- src/java/com/flowserve/system606/service/AppInitializeService.java
+++ src/java/com/flowserve/system606/service/AppInitializeService.java
@@ -6,6 +6,11 @@
 package com.flowserve.system606.service;
 
 import com.flowserve.system606.model.User;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -72,6 +77,73 @@ public class AppInitializeService {
         }
 
         logger.info("Initializing App Objects Done");
+    }
+
+    public void initContract(String msAccDB) {
+        Logger.getLogger(AppInitializeService.class.getName()).log(Level.INFO, "Processing initContract: " + msAccDB);
+ 
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+ 
+        // Step 1: Loading or registering Oracle JDBC driver class
+        try {
+ 
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+        }
+        catch(ClassNotFoundException cnfex) {
+ 
+            System.out.println("Problem in loading or "
+                    + "registering MS Access JDBC driver");
+            cnfex.printStackTrace();
+        }
+ 
+        // Step 2: Opening database connection
+        try {
+ 
+            String dbURL = "jdbc:ucanaccess://" + msAccDB; 
+ 
+            // Step 2.A: Create and get connection using DriverManager class
+            connection = DriverManager.getConnection(dbURL); 
+ 
+            // Step 2.B: Creating JDBC Statement 
+            statement = connection.createStatement();
+ 
+            // Step 2.C: Executing SQL &amp; retrieve data into ResultSet
+            resultSet = statement.executeQuery("SELECT * FROM Contract");
+ 
+            Logger.getLogger(AppInitializeService.class.getName()).log(Level.INFO, "ID\tTemplate\t\t\tName\tStage");
+            Logger.getLogger(AppInitializeService.class.getName()).log(Level.INFO, "==\t================\t===\t=======");
+ 
+            // processing returned data and printing into console
+            while(resultSet.next()) {
+                Logger.getLogger(AppInitializeService.class.getName()).log(Level.INFO, resultSet.getInt(1) + "\t" + 
+                        resultSet.getString(2) + "\t" + 
+                        resultSet.getString(3) + "\t" +
+                        resultSet.getString(4));
+            }
+        }
+        catch(SQLException sqlex){
+            sqlex.printStackTrace();
+        }
+        finally {
+ 
+            // Step 3: Closing database connection
+            try {
+                if(null != connection) {
+ 
+                    // cleanup resources, once after processing
+                    resultSet.close();
+                    statement.close();
+ 
+                    // and then finally close connection
+                    connection.close();
+                }
+            }
+            catch (SQLException sqlex) {
+                sqlex.printStackTrace();
+            }
+        }
     }
 
 }
